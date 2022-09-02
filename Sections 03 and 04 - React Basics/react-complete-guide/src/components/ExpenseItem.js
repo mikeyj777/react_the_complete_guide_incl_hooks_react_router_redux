ee87c4a56033b84c9254b59928c7087cd9e492df
Michael James <mikeyj777@gmail.com>
2022-09-02 14:53:01 -0400

    Lect 37 - "Props" to pass data

--- Sections 03 and 04 - React Basics/react-complete-guide/src/components/ExpenseItem.js
+++ Sections 03 and 04 - React Basics/react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import './css/ExpenseItem.css';
+import "./css/ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     // only 1 root div
     return (
         <div className="expense-item">
-            <div>March 28, 2021</div>
-            <h2 className="expense-item__description">Car Insurance</h2>
-            <div className="expense-item__price">$294.17</div>
+            <div> {props.date.toLocaleDateString()} </div>
+            <h2 className="expense-item__description"> {props.title} </h2>
+            <div className="expense-item__price">${props.amount} </div>
         </div>
     );
 }
